ce51b6e63dace43bf69c35b86f9bf74f0ad1b984
RyanDN6 <149425630+RyanDN6@users.noreply.github.com>
2024-04-10 18:45:59 +0100

    Updated play.py massively

--- play.py
+++ play.py
@@ -1,27 +1,106 @@
-import classes
-import weapons
-import skills
+import classes, pyfiglet, winsound, playsound
+from time import sleep
+from classes import attack
+
+colours = {
+    'red': "31",
+    'green': "32",
+    'yellow': "33",
+    'blue': "34",
+    'purple': "35",
+    'cyan': "36",
+    'white': "0",
+}
+
+
+winsound.PlaySound("background.wav", winsound.SND_LOOP + winsound.SND_ASYNC)
 
 def confirm():
-    a = input()
+    a = ""
     while a not in ["y", "n"]:
-        print("Confirm? (y/n)")
+        text(words="Confirm? (y/n)", colour='green', sound=True)
         a = input()
-    return a
+    if a.lower() == "y":
+        return True
+    else:
+        return False
 
-print("Choose your class:")
-options = ["===================", "Barbarian", "Pirate", "Samaurai", "Paladin", "Assassin", "==================="]
-print("\n".join(options))
-print("Type a name to view more.\n")
-choice = input().capitalize()
+def text(words="", delay=0.04, colour='white', sound=False):
+    for char in words:
+        if sound:
+            playsound.playsound(sound="sounds/text.mp3", block=False)
+        sleep(delay)
+        print(f"\033[1;{colours[colour]};40m{char}", end="", flush=True)
+        if char in ".,!?":
+            sleep(delay * 5)
+    print("\n")
 
-while choice not in options:
-    print("Type a name to view more.")
+while True:
+    try:
+        text("Choose your scroll speed:")
+        print("1 (FASTEST)")
+        print("2 (FASTER)")
+        print("3 (FAST)")
+        print("4 (NORMAL)")
+        print("5 (SLOW)")
+        print("6 (SLOWER)")
+        print("7 (SLOWEST)")
+        n = int(input())
+        if n >= 1 and n <= 7:
+            scrollSpeed = float(f"0.0{n}")
+            text(words="This is a sample text for you to see your chosen speed. If you would like to change your choice, type 'n'. Otherwise, type 'y' to confirm.", delay=scrollSpeed, sound=True)
+            a = confirm()
+            if a:
+                scrollSpeed = float(f"0.0{n}")
+                break
+            else:
+                continue
+    except ValueError:
+        continue
+
+def text(words="", delay=scrollSpeed, colour='white', sound=False):
+    for char in words:
+        if sound:
+            playsound.playsound(sound="sounds/text.mp3", block=False)
+        sleep(delay)
+        print(f"\033[1;{colours[colour]};40m{char}", end="", flush=True)
+        if char in ".,!?":
+            sleep(delay * 5)
+    sleep(delay * 5)
+    print("\n")
+
+menu = True
+
+while menu:
+    text(pyfiglet.figlet_format("Draayln's Veil"), delay=0.005, colour='blue')
+    sleep(2)
+    break
+
+boolean = True
+while boolean:
+    text(words="Choose your class:", sound=True)
+    options = ["===================", "Barbarian", "Pirate", "Samaurai", "Paladin", "Assassin", "==================="]
+    text(words="\n".join(options))
+    text(words="Type a name to view more.", sound=True)
     choice = input().capitalize()
 
-choice = getattr(classes, choice)()
-print("\n" + choice.info + "\n")
+    while choice not in options:
+        text("Type a name to view more.")
+        choice = input().capitalize()
 
-print("Confirm your class? (y/n)")
-a = confirm()
-print(a)
+    character = getattr(classes, choice)()
+    text(f'\n{character.info}\nWeapon: {character.startingWeapon.name}\n')
+
+    if confirm():
+        text(words=f"You've chosen the {choice}.", colour='red', sound=True)
+        sleep(2)
+        boolean = False
+
+text("Would you like a quick tutorial?")
+
+if confirm():
+    while True:
+        text("Add tutorial later")
+        break
+else:
+    text("Choose which path")
