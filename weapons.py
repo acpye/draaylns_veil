64b2f6ef5b2b343fab52b8fdef8bb67a6d983dda
RyanDN6 <149425630+RyanDN6@users.noreply.github.com>
2024-04-10 12:44:22 +0100

    weapons.py blueprint finished

--- weapons.py
+++ weapons.py
@@ -1,30 +1,23 @@
 class Weapon:
-    def __init__(self, name, damage=0, procCoefficient=0, staminaConsumption=0, stun=False, poison=False, bleed=False):
+    def __init__(self, name, damage=0, damageRoll=0, procCoefficient=0, staminaConsumption=0, stun=False, poison=False, bleed=False):
         self.name = name
         self.damage = damage
         self.procCoefficient = procCoefficient
         self.staminaConsumption = staminaConsumption
+        self.stun = stun
+        self.poison = poison
+        self.bleed = bleed
 
-weaponsList = [
-    Weapon(name="Greatsword", damage=0, procCoefficient=0, staminaConsumption=0),
-    Weapon(name="Hammer", damage=0, procCoefficient=0, staminaConsumption=0, stun=True),
-    Weapon(name="Battleaxe", damage=0, procCoefficient=0, staminaConsumption=0),
-    Weapon(name="Morning Star", damage=0, procCoefficient=0, staminaConsumption=0),
-    Weapon(name="Sword", damage=0, procCoefficient=0, staminaConsumption=0),
-    Weapon(name="Katana", damage=0, procCoefficient=0, staminaConsumption=0, bleed=True),
-    Weapon(name="Quarterstaff", damage=0, procCoefficient=0, staminaConsumption=0),
-    Weapon(name="Club", damage=0, procCoefficient=0, staminaConsumption=0, stun=True),
-    Weapon(name="Dagger", damage=0, procCoefficient=0, staminaConsumption=0, bleed=True),
-    Weapon(name="Poison-Tipped Dagger", damage=0, procCoefficient=0, staminaConsumption=0, poison=True),
-    Weapon(name="Hook", damage=0, procCoefficient=0, staminaConsumption=0, bleed=True)
-]
-
-# procCoefficient is there for things like poison or stuns
-
-
-
-
-
-
-
-
+weaponsList = {
+    "Greatsword": Weapon(name = "Greatsword", damage=10, damageRoll=6, procCoefficient=0, staminaConsumption=5),
+    "Battleaxe": Weapon(name="Battleaxe", damage=8, damageRoll=6, procCoefficient=0, staminaConsumption=4),
+    "Hammer": Weapon(name="Hammer", damage=7, damageRoll=4, procCoefficient=0, staminaConsumption=4, stun=True),
+    "Morning Star": Weapon(name="Morning Star", damage=7, damageRoll=7, procCoefficient=0, staminaConsumption=4),
+    "Sword": Weapon(name="Sword", damage=6, damageRoll=5, procCoefficient=0, staminaConsumption=3),
+    "Katana": Weapon(name="Katana", damage=5, damageRoll=3, procCoefficient=0, staminaConsumption=3, bleed=True),
+    "Quaterstaff": Weapon(name="Quarterstaff", damage=5, damageRoll=6, procCoefficient=0, staminaConsumption=3),
+    "Club": Weapon(name="Club", damage=4, damageRoll=4, procCoefficient=0, staminaConsumption=3, stun=True),
+    "Dagger": Weapon(name="Dagger", damage=3, damageRoll=2, procCoefficient=0, staminaConsumption=2, bleed=True),
+    "Poison-Tipped Dagger": Weapon(name="Poison-Tipped Dagger", damage=0, damageRoll=2, procCoefficient=0, staminaConsumption=1, poison=True),
+    "Hook": Weapon(name="Hook", damage=0, damageRoll=2, procCoefficient=0, staminaConsumption=2, bleed=True),   
+}
