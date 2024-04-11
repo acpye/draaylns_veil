fe0dc2c360e829dd7ae204ed395824cdd74edcfc
RyanDN6 <149425630+RyanDN6@users.noreply.github.com>
2024-04-11 22:43:10 +0100

    Update classes.py

--- classes.py
+++ classes.py
@@ -1,9 +1,14 @@
+import weapons
+from health import HealthBar
+
 class Barbarian(object):
 
-    def __init__(self, maxHitpoints):
+    def __init__(self, name="Barbarian"):
+        self.ally = True
+        self.name = name
         self.info = "The Barbarian is a kilt-clad warrior hungry for destruction. Big damage and health numbers, little stamina."
-        self.maxHitpoints = 0
-        self.hitpoints = maxHitpoints
+        self.maxHitpoints = 115
+        self.hitpoints = self.maxHitpoints
         self.defence = 0 # Armour value can override this
         self.strength = 0
         self.attack = 0 # If we set this to something low it can act as fists if player lacks weapon - weapon damage can just override this
@@ -11,13 +16,20 @@ class Barbarian(object):
         self.evasion = 0 # Should stay relatively low by default with few skills that can affect - maybe armour can also affect?
         self.luck = 0 # will affect proc chance. affects chance of increased level up rewards? affects victory drop rates?
         self.skill = "Bloodrage" # We need to make sure certain skills do not turn up in the random roll so should probably assign them here instead
+        self.weapon = weapons.weaponsList["Battleaxe"]
+        self.healthBar = HealthBar(self, color="green")
+        self.stamina = 10
+        self.maxStamina = self.stamina
+        self.staminaRecovery = 2
 
-class Samaurai(object):
+class Samurai(object):
 
-    def __init__(self, maxHitpoints):
-        self.info = "The Samaurai is..."
-        self.maxHitpoints = 0
-        self.hitpoints = maxHitpoints
+    def __init__(self, name="Samurai"):
+        self.ally = True
+        self.name = name
+        self.info = "The Samurai is a disciplined killer, adept and honourable."
+        self.maxHitpoints = 95
+        self.hitpoints = self.maxHitpoints
         self.defence = 0
         self.strength = 0
         self.attack = 0
@@ -25,14 +37,20 @@ class Samaurai(object):
         self.evasion = 0
         self.luck = 0
         self.skill = "Withhold"
-
+        self.weapon = weapons.weaponsList["Katana"]
+        self.healthBar = HealthBar(self, color="green")
+        self.stamina = 10
+        self.maxStamina = self.stamina
+        self.staminaRecovery = 2
 
 class Pirate(object):
 
-    def __init__(self, maxHitpoints):
+    def __init__(self, name="Pirate"):
+        self.ally = True
+        self.name = name
         self.info = "The Pirate is a plundering outlaw, experienced with combat and their cutlass."
-        self.maxHitpoints = 0
-        self.hitpoints = maxHitpoints
+        self.maxHitpoints = 105
+        self.hitpoints = self.maxHitpoints
         self.defence = 0
         self.strength = 0
         self.attack = 0
@@ -40,13 +58,20 @@ class Pirate(object):
         self.evasion = 0
         self.luck = 0
         self.skill = "Plunder"
+        self.weapon = weapons.weaponsList["Hook"]
+        self.healthBar = HealthBar(self, color="green")
+        self.stamina = 10
+        self.maxStamina = self.stamina
+        self.staminaRecovery = 2
 
 class Paladin(object):
 
-    def __init__(self, maxHitpoints):
+    def __init__(self, name="Paladin"):
+        self.ally = True
+        self.name = name
         self.info = "The Paladin is the King's loyal subject, a strong and stoic knight"
-        self.maxHitpoints = 0
-        self.hitpoints = maxHitpoints
+        self.maxHitpoints = 125
+        self.hitpoints = self.maxHitpoints
         self.defence = 0
         self.strength = 0
         self.attack = 0
@@ -54,13 +79,20 @@ class Paladin(object):
         self.evasion = 0
         self.luck = 0
         self.skill = "Barrier"
+        self.weapon = weapons.weaponsList["Hammer"]
+        self.healthBar = HealthBar(self, color="green")
+        self.stamina = 10
+        self.maxStamina = self.stamina
+        self.staminaRecovery = 2
 
 class Assassin(object):
 
-    def __init__(self, maxHitpoints):
+    def __init__(self, name="Assassin"):
+        self.ally = True
+        self.name = name
         self.info = "The Assassin is a lethal and deadly hunter. Fast attacks with a high chance for critical hits."
-        self.maxHitpoints = 0
-        self.hitpoints = maxHitpoints
+        self.maxHitpoints = 80
+        self.hitpoints = self.maxHitpoints
         self.defence = 0
         self.strength = 0
         self.attack = 0
@@ -68,7 +100,10 @@ class Assassin(object):
         self.evasion = 0
         self.luck = 0
         self.skill = "Backstab"
-
-# need stamina function (need to decide which model to use as will affect rest of code + balancing)
+        self.weapon = weapons.weaponsList["Dagger"]
+        self.healthBar = HealthBar(self, color="green")
+        self.stamina = 10
+        self.maxStamina = self.stamina
+        self.staminaRecovery = 2
+    
 # need death function (i think permadeath as can recruit more allies - if og character dies should STILL continue on with allies)
-# need run away function (potentially affected by luck or agility? more likely agility)
