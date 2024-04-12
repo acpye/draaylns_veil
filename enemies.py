e4c92f492eb13416090e5a7b02bffba2f3c516cb
RyanDN6 <149425630+RyanDN6@users.noreply.github.com>
2024-04-12 20:41:31 +0100

    Update enemies.py
    using class inheritance, we can cleanly make separate instances of the same class with little hassle

--- enemies.py
+++ enemies.py
@@ -1,25 +1,21 @@
 import weapons
 class Enemy:
-    def __init__(self, name, ally=False, attack=0, hitpoints=0, damageRoll=0, stamina=0, staminaRecovery=0, procCoefficient=0, staminaConsumption=0, stun=False, poison=False, bleed=False, weapon="Fists"):
+    def __init__(self, name, ally=False, attack=0, hitpoints=0, stamina=0, staminaRecovery=0, weapon="Fists"):
         self.name = name
         self.ally = ally
         self.attack = attack
-        self.procCoefficient = procCoefficient
-        self.staminaConsumption = staminaConsumption
-        self.stun = stun
-        self.poison = poison
-        self.bleed = bleed
         self.hitpoints = hitpoints
         self.maxHitpoints = hitpoints
-        self.damageRoll= damageRoll
-        self.stun = stun
-        self.bleed = bleed
-        self.poison = poison
         self.weapon = weapons.weaponsList[weapon]
         self.stamina = stamina
         self.maxStamina = self.stamina
         self.staminaRecovery = staminaRecovery
 
-enemyList = {
-    "Goblin": Enemy(name="Goblin", hitpoints=40, attack=1, stamina=6, staminaRecovery=1),  
-}
+class Goblin(Enemy):
+    def __init__(self):
+        super().__init__(name="Goblin", attack=1, hitpoints=25, stamina=18)
+
+class Dragon(Enemy):
+    def __init__(self):
+        super().__init__(name="Dragon", attack=15, hitpoints=100, stamina=20)
+        
