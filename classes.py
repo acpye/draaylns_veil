e427b8a36eb57103184b844aea3fe1c9e5a3128f
RyanDN6 <149425630+RyanDN6@users.noreply.github.com>
2024-04-12 21:29:41 +0100

    Update classes.py
    INHERIT AND SHIT

--- classes.py
+++ classes.py
@@ -1,109 +1,40 @@
 import weapons
-from health import HealthBar
 
-class Barbarian(object):
-
-    def __init__(self, name="Barbarian"):
-        self.ally = True
+class Character(object):
+    def __init__(self, ally=True, name="", info="", maxHitpoints=100, defence=0, attack=0, agility=0, evasion=0, luck=0, skill="", weapon="Fists", stamina=20, staminaRecovery=2):
+        self.skill = skill
+        self.skills = [skill]
+        self.ally = ally
         self.name = name
-        self.info = "The Barbarian is a kilt-clad warrior hungry for destruction. Big damage and health numbers, little stamina."
-        self.maxHitpoints = 115
+        self.info = info
+        self.maxHitpoints = maxHitpoints
         self.hitpoints = self.maxHitpoints
-        self.defence = 0 # Armour value can override this
-        self.strength = 0
-        self.attack = 0 # If we set this to something low it can act as fists if player lacks weapon - weapon damage can just override this
-        self.agility = 0
-        self.evasion = 0 # Should stay relatively low by default with few skills that can affect - maybe armour can also affect?
-        self.luck = 0 # will affect proc chance. affects chance of increased level up rewards? affects victory drop rates?
-        self.skill = "Bloodrage" # We need to make sure certain skills do not turn up in the random roll so should probably assign them here instead
-        self.weapon = weapons.weaponsList["Battleaxe"]
-        self.healthBar = HealthBar(self, color="green")
-        self.stamina = 10
+        self.defence = defence
+        self.attack = attack
+        self.agility = agility
+        self.evasion = evasion
+        self.luck = luck
+        self.weapon = weapons.weaponsList[weapon]
+        self.stamina = stamina
         self.maxStamina = self.stamina
-        self.staminaRecovery = 2
+        self.staminaRecovery = staminaRecovery
+
+class Barbarian(Character):
+    def __init__(self):
+        super().__init__(name="Barbarian", info="The Barbarian is a kilt-clad warrior hungry for destruction. Big damage and health numbers, little stamina.", maxHitpoints=115, weapon="Battleaxe", skill="Bloodrage")
 
 class Samurai(object):
-
-    def __init__(self, name="Samurai"):
-        self.ally = True
-        self.name = name
-        self.info = "The Samurai is a disciplined killer, adept and honourable."
-        self.maxHitpoints = 95
-        self.hitpoints = self.maxHitpoints
-        self.defence = 0
-        self.strength = 0
-        self.attack = 0
-        self.agility = 0
-        self.evasion = 0
-        self.luck = 0
-        self.skill = "Withhold"
-        self.weapon = weapons.weaponsList["Katana"]
-        self.healthBar = HealthBar(self, color="green")
-        self.stamina = 10
-        self.maxStamina = self.stamina
-        self.staminaRecovery = 2
+    def __init__(self):
+        super().__init__(name="Samurai", info="The Samurai is a disciplined killer, adept and honourable.", maxHitpoints=95, weapon="Katana", skill="Withhold")
 
 class Pirate(object):
-
-    def __init__(self, name="Pirate"):
-        self.ally = True
-        self.name = name
-        self.info = "The Pirate is a plundering outlaw, experienced with combat and their cutlass."
-        self.maxHitpoints = 105
-        self.hitpoints = self.maxHitpoints
-        self.defence = 0
-        self.strength = 0
-        self.attack = 0
-        self.agility = 0
-        self.evasion = 0
-        self.luck = 0
-        self.skill = "Plunder"
-        self.weapon = weapons.weaponsList["Hook"]
-        self.healthBar = HealthBar(self, color="green")
-        self.stamina = 10
-        self.maxStamina = self.stamina
-        self.staminaRecovery = 2
+    def __init__(self):
+        super().__init__(name="Pirate",info="The Pirate is a plundering outlaw, experienced with combat and their cutlass.", maxHitpoints=105, weapon="Hook", skill="Plunder")
 
 class Paladin(object):
-
-    def __init__(self, name="Paladin"):
-        self.ally = True
-        self.name = name
-        self.info = "The Paladin is the King's loyal subject, a strong and stoic knight"
-        self.maxHitpoints = 125
-        self.hitpoints = self.maxHitpoints
-        self.defence = 0
-        self.strength = 0
-        self.attack = 0
-        self.agility = 0
-        self.evasion = 0
-        self.luck = 0
-        self.skill = "Barrier"
-        self.weapon = weapons.weaponsList["Hammer"]
-        self.healthBar = HealthBar(self, color="green")
-        self.stamina = 10
-        self.maxStamina = self.stamina
-        self.staminaRecovery = 2
+    def __init__(self):
+        super().__init__(name="Paladin", info="The Paladin is the King's loyal subject, a strong and stoic knight", maxHitpoints=125, weapon="Hammer", skill="Barrier")
 
 class Assassin(object):
-
-    def __init__(self, name="Assassin"):
-        self.ally = True
-        self.name = name
-        self.info = "The Assassin is a lethal and deadly hunter. Fast attacks with a high chance for critical hits."
-        self.maxHitpoints = 80
-        self.hitpoints = self.maxHitpoints
-        self.defence = 0
-        self.strength = 0
-        self.attack = 0
-        self.agility = 0
-        self.evasion = 0
-        self.luck = 0
-        self.skill = "Backstab"
-        self.weapon = weapons.weaponsList["Dagger"]
-        self.healthBar = HealthBar(self, color="green")
-        self.stamina = 10
-        self.maxStamina = self.stamina
-        self.staminaRecovery = 2
-    
-# need death function (i think permadeath as can recruit more allies - if og character dies should STILL continue on with allies)
+    def __init__(self):
+        super().__init__(name="Assassin", info="The Assassin is a lethal and deadly hunter. Fast attacks with a high chance for critical hits.", maxHitpoints=80, weapon="Dagger", skill="Backstab")
